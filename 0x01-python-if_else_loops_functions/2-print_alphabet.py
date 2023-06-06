758e0d680bab10b2a22662d3d982c3f0b116cc0f
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-06 14:04:05 -0700

    Task 3

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for j in range(ord('a'), ord('z')+1):
+    print(chr(j), end='')
+
+print()
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for j in range(ord('a'), ord('z')+1):
+    if chr(j) not in ['q', 'e']:
+        print(chr(j), end='')
+
+print()
