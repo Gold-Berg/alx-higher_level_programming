82199561218754d671669d5d80d84ea605da0846
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-07 16:57:05 -0700

    task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a')-1 , -1):
-    print("{}{}".format(chr(i), chr(i-32)), end="")
+    print("{}".format(chr(i- j)), end="")
+    j = 32 if j == 0 else 0
