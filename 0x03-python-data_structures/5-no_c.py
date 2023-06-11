df11cd96429255a5efb551c4094d65b5da8921aa
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-11 09:28:28 -0700

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
+    new = []
     for char in my_string:
-        if char != 'c' or char != 'C':
-            new = new + char
-    return new
+        if char not in ('c', 'C'):
+            new.append(char)
+    return ''.join(new_string)
