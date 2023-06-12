c6afcf78f74e7c937b403443e31f24a5511947c5
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-12 16:57:22 -0700

    Task 100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,7 +2,20 @@
 
 void print_python_list_info(PyObject *p);
 {
+	const char *type;
 	Py_ssize_t size = PyList_Size(p);
+
 	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
 
-	printf("{*} Size of the python List = %zd")
+	printf("{*} Size of the python List = %zd\n" size);
+	printf("[*] Allocated = %zd\n", allocated);
+
+	for (Py_ssize_t i = 0; i < size; i++)
+	{
+		PyObject *element = PyList_GetTtem(p, i);
+
+		type = Py_TYPE(element)->tp_name;
+
+		printf("Element %zd: %s\n", i, type);
+	}
+}
