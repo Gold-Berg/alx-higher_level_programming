cba30334211963cad97e4ff4f400ceb1dad3bc5e
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-14 16:57:55 -0700

    Task 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     if value not in a_dictionary.values():
         return a_dictionary
-    _delete = [key for key, v1 in a_dictionary.items() if v1 == value]
-    for key in_delete:
+    _delete = [key for key, vl in a_dictionary.items() if vl == value]
+    for key in _delete:
         del a_dictionary[key]
-        return a_dictionary
+    return a_dictionary
