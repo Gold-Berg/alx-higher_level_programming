e46fca74bb02b29817c37d90ca68f2cd8d98a791
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-14 14:51:04 -0700

    Task 1_1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    aws = [[0] * len(row) for row in matrix]
+    result = [[0] * len(row) for row in matrix]
 
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            aws[i][j] = matrix[i][j] ** 2
+            result[i][j] = matrix[i][j] ** 2
 
-    return aws
+    return result
