6f102609bb15c496b1dc72b6f020c13b82178f6e
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-26 15:07:56 -0700

    Task 6

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,4 @@ def safe_print_list_integers(my_list=[], x=0):
                 break
         except (ValueError, TypeError):
             continue
-    except (IndexError):
-        pass
     return count
