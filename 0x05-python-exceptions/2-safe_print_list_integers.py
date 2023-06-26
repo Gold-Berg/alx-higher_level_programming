ab3a93642754a657ab5b688f8db1a4e6edce97fd
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-26 13:52:18 -0700

    Task 2.2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             track += 1
             if track == x:
                 break
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             continue
     print()
     return track
