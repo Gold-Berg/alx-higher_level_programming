abd070a3943af64e0ef3c59cf568ed78b9b01d24
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-27 14:16:40 -0700

    Task 1 & 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module of a square class """
+class Square:
+    """ Represent a square class """
+    def __init__(self, size):
+        """ square instance with a given size """
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+All 0x06-python-classes Task
