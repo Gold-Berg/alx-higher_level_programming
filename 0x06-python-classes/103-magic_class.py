2a6a3a7bc8b5764267f3b94c17aa7d84f539ab10
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-27 18:47:40 -0700

    Task 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ bytecode interpretation"""
 import math
-
+""" import math mordule"""
 class MagicClass:
+    """ magicclass class """
+
+
     def __init__(self, radius):
+        """ instances of class """
         self.__radius = 0
 
         if type(radius) is not int and type(radius) is not float:
@@ -12,7 +16,9 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
+        """ returns area"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """ returns circumfrence """
         return 2 * math.pi * self.__radius
