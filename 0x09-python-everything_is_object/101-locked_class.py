7c60cf17832f11c51329af83fe8a100bfab62245
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-07-04 17:01:36 -0700

    task 101

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module of class """
+
+class LockedClass:
+    __slots__ = ('first_name',)
+
+    def __setattr__(self, attr, value):
+        if attr != 'first_name':
+            raise AttributeError("Cannot dynamically create new instance attributes")
+        super().__setattr__(attr, value)
