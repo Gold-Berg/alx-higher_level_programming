cb5d8f7452ce7522970362a6b6d51795389d7fc3
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-12-10 18:39:23 -0800

    Task 2.2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,8 +6,8 @@ checks if obj is an instace
 
 def is_exact_instance(obj, class_type):
     """
-
     Return true if obj is an innstance else false
+
     """
 
     return isinstance(obj, class_type and type(obj) is class_type)
