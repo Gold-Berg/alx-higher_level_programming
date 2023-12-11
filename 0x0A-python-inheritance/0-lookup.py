275b5dce15436749b38d777f7db0f2e1c198db44
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-12-10 16:48:58 -0800

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+A function that returns the list of available attributes/methods of an object:
+"""
+
+
+def lookup(obj):
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+Contains all alx 0x0A-python-inheritance Task
