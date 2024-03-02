2807db88c1354e04706e9c40944ba5385616d445
Gold-Berg <igbinobaeseosa0@gmail.com>
2024-03-01 20:59:31 -0800

    Task 7.9

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+def main():
+    """Main function"""
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    data = {'q': letter}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+if __name__ == "__main__":
+    main()
+
