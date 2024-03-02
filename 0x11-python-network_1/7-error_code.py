65498a0463dd8264f692366e7e3d8ef08e446ca5
Gold-Berg <igbinobaeseosa0@gmail.com>
2024-03-01 20:57:47 -0800

    Task 7.8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """
-Python script that sends a request to the URL and
-displays:
-- The body of the response if there are no errors
+This script takes a URL as input, sends a request to the URL, and displays the body of the response.
+If the HTTP status code is greater than or equal to 400, it prints an error message with the status code.
+"""
 
 import requests
 import sys
 
-if __name__ == "__main__":
+def main():
+    """Main function"""
     url = sys.argv[1]
     response = requests.get(url)
 
     if response.status_code >= 400:
-        print("Error code:", response.status_code)
+        print(f"Error code: {response.status_code}")
     else:
         print(response.text)
+
+if __name__ == "__main__":
+    main()
 
