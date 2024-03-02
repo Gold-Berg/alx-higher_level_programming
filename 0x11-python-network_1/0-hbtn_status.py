b5498a6d94b777941d29a2d42fea1e99b2e65648
Gold-Berg <igbinobaeseosa0@gmail.com>
2024-03-01 20:01:34 -0800

    Task 0-3

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body_response = response.read()
+    print("Body response:")
+    print("\t- type:", type(body_response))
+    print("\t- content:", body_response)
+    print("\t- utf8 content:", body_response.decode('utf-8'))
+
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+All 0x11-python-network_1 Task
